ca35cac0d10930aa4f09ba0078279bb7771a5b7d
Jurgita Butiene <jurgita02@yahoo.com>
2023-07-27 18:20:22 +0200

    done with react Pizza maker

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import Pizza from './Pizza/Pizza';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <>
+
+  <Pizza/>
     <App />
-  </React.StrictMode>
+
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
